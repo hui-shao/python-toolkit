8dfd253310d4d7d4ef302622d37a724f0b2a7185
hui-shao <xhzrzb@163.com>
2020-02-04 23:31:27 +0800

    优化文案和一些细节

--- JD-stock-monitor.py
+++ JD-stock-monitor.py
@@ -12,15 +12,15 @@ def sendMail(name,url,timestamp):
 
     # 用于构建邮件头
 
-    # 发信方的信息：发信邮箱，邮箱授权码
-    from_addr = '<your email address>@xxx.com'
-    password = 'your verify code'
+    # 发信方的信息：发信邮箱，QQ 邮箱授权码
+    from_addr = '<your email address>@example.com'
+    password = '<your verify code>'
 
     # 收信方邮箱
     to_addr = mail_receiver
 
     # 发信服务器
-    smtp_server = 'smtp.xxx.com'
+    smtp_server = '<your smtp address>'
 
     # 邮箱正文内容，第一个参数为内容，第二个参数为格式(plain 为纯文本)，第三个参数为编码
     msg = MIMEText("你关注的商品：\n\n"+name+"："+url+"\n\n现在有货啦，赶快去购买吧\n"+timestamp,"plain","utf-8")
@@ -69,15 +69,17 @@ flag = 0
 # 判断系统类型
 sys_type = platform.system()
 # 收件邮箱（多个用,分割）
-mail_receiver = 'example1@gmail.com,example2@163.com,example3@qq.com'
+mail_receiver = 'example@163.com,example@qq.com'
 # 商品的url
 urls = [
     'https://c0.3.cn/stock?skuId=1336984&area=16_1370_46145_0&venderId=1000078145&buyNum=1&choseSuitSkuIds=&cat=9192,12190,1517&extraParam={%22originid%22:%221%22}&fqsp=0&pdpin=&pduid=158071785741420407990&ch=1&callback=jQuery1571998',
     'https://c0.3.cn/stock?skuId=100011293950&area=16_1370_46145_0&venderId=1000078145&buyNum=1&choseSuitSkuIds=&cat=9192,12190,1517&extraParam={%22originid%22:%221%22}&fqsp=0&pdpin=&pduid=158071785741420407990&ch=1&callback=jQuery1314477',
-    'https://c0.3.cn/stock?skuId=1163651&area=16_1370_46145_0&venderId=1000078145&buyNum=1&choseSuitSkuIds=&cat=9192,12190,12602&extraParam={%22originid%22:%221%22}&fqsp=0&pdpin=&pduid=158071785741420407990&ch=1&callback=jQuery5088839'
+    'https://c0.3.cn/stock?skuId=100011293928&area=16_1370_46145_0&venderId=1000078145&buyNum=1&choseSuitSkuIds=&cat=9192,12190,1517&extraParam={%22originid%22:%221%22}&fqsp=0&pdpin=&pduid=158071785741420407990&ch=1&callback=jQuery3089493',
+    'https://c0.3.cn/stock?skuId=100006248177&area=16_1370_46145_55521&venderId=1000078145&buyNum=1&choseSuitSkuIds=&cat=9192,12190,1517&extraParam={%22originid%22:%221%22}&fqsp=0&pdpin=&pduid=158071785741420407990&ch=1&callback=jQuery4539079',
+    'https://c0.3.cn/stock?skuId=100006248247&area=16_1370_46145_55521&venderId=1000078145&buyNum=1&choseSuitSkuIds=&cat=9192,12190,1517&extraParam={%22originid%22:%221%22}&fqsp=0&pdpin=&pduid=158071785741420407990&ch=1&callback=jQuery8283487'
     ]
 # 每个url对应名称(方便辨认)
-names = ['D7002-H930','H950V-,25只1盒','耳塞']
+names = ['D7002 蓝色 10只','H950V 白色 25只','H930V 白色 5只','D7002V 蓝色 6只','D7030V 白色 6只']
 
 while (1):
     try:
@@ -88,22 +90,30 @@ while (1):
             "Connection": "keep-alive"
         }
         time_now = time.strftime("%Y-%m-%d %H:%M:%S",time.localtime())
-        print('第' + str(flag) + '次    ------    ' + time_now)
+        print('### 第 ' + str(flag) + ' 次         ' + time_now)
         flag += 1
         a = 0 # 用于匹配names列表的索引号
         for url in urls:
             # 商品url
             skuidUrl = 'https://item.jd.com/' + url.split('skuId=')[1].split('&')[0] + '.html'
             name = names[a]
-            response = session.get(url)
-            #print(response.text)
-            if (response.text.find('无货') > 0):
-                print('无货:' +name+'：' +skuidUrl)
-            else:
-                print('有货-有货- 有货啦!:'+name+'：  '+ skuidUrl)
-                sendMail(name,skuidUrl,time_now)
+            try:
+                response = session.get(url)
+                #print(response.text)
+                if (response.text.find('无货') > 0):
+                    print('无货:' +name+'：' +skuidUrl)
+                else:
+                    print('有货-有货- 有货啦!:'+name+'：  '+ skuidUrl)
+                    sendMail(name,skuidUrl,time_now)
+                    # 有货时把响应保存一下(ps.主要还是调试用)
+                    f = open("./%s-%s.txt" % (flag,time.localtime()),"w",encoding="utf-8")
+                    f.write(response.text)
+                    f.close()
+            except requests.exceptions.ConnectionError:
+                print("网络连接错误")
+                time.sleep(3)
             a = a + 1
-        time.sleep(random.randint(8,30))
+        time.sleep(random.randint(10,15))
         clean()
     except KeyboardInterrupt:
         print("\n用户终止操作！程序退出...")
