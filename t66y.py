76ef0a937436a9bcea725d8f831046248f4f9919
hui-shao <31568606+hui-shao@users.noreply.github.com>
2020-02-16 15:59:51 +0800

    添加 单独输出地址 以及 规范代码

--- t66y.py
+++ t66y.py
@@ -1,8 +1,13 @@
 import requests
+import json
 
 url = "https://get.xunfs.com/app/listapp.php"
-hea = {"User-Agent":"Mozilla/4.0 compatible; MSIE 6.13; Windows NT 5.1;SV1","Accept-Encoding":"gzip"}
-par = {"a":"get18","system":"android","v":"2.2.4"}
-
-r = requests.post(url,headers=hea,data=par)
-print(r.text)
+hea = {"User-Agent": "Mozilla/4.0 compatible; MSIE 6.13; Windows NT 5.1;SV1", "Accept-Encoding": "gzip"}
+par = {"a": "get18", "system": "android", "v": "2.2.4"}
+r = requests.post(url, headers=hea, data=par)
+dic = json.loads(r.text)
+print("原响应：\n%s\n" % (dic))
+print("地址：")
+print(dic["url1"])
+print(dic["url2"])
+print("%s\n" % (dic["url3"]))
