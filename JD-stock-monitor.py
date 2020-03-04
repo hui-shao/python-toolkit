5b343faef2cb3949000fb4f7ce006534d67c2e57
hui-shao <31568606+hui-shao@users.noreply.github.com>
2020-03-04 10:54:16 +0800

    新增 server酱消息推送    格式化代码

--- JD-stock-monitor.py
+++ JD-stock-monitor.py
@@ -4,7 +4,8 @@ import os
 import platform
 import random
 
-def sendMail(name,url,timestamp):
+
+def sendMail(name, url, timestamp):
     import smtplib
     from email.mime.text import MIMEText
     # email 用于构建邮件内容
@@ -23,7 +24,7 @@ def sendMail(name,url,timestamp):
     smtp_server = '<your smtp address>'
 
     # 邮箱正文内容，第一个参数为内容，第二个参数为格式(plain 为纯文本)，第三个参数为编码
-    msg = MIMEText("你关注的商品：\n\n"+name+"："+url+"\n\n现在有货啦，赶快去购买吧\n"+timestamp,"plain","utf-8")
+    msg = MIMEText("你关注的商品：\n\n"+name+"："+url+"\n\n现在有货啦，赶快去购买吧\n"+timestamp, "plain", "utf-8")
 
     # 邮件头信息
     msg['From'] = Header(from_addr)
@@ -33,7 +34,7 @@ def sendMail(name,url,timestamp):
 
     try:
         # 开启发信服务，这里使用的是加密传输
-        server = smtplib.SMTP_SSL(smtp_server) # 不传入参数似乎会报错
+        server = smtplib.SMTP_SSL(smtp_server)  # 不传入参数似乎会报错
         server.connect(smtp_server, 465)
         # 登录发信邮箱
         server.login(from_addr, password)
@@ -51,6 +52,22 @@ def sendMail(name,url,timestamp):
     except smtplib.SMTPException as e:
         print(e.message)
 
+
+def send_wxmsg(_sckey, _title="标题", _context="正文"):
+    url = "https://sc.ftqq.com/%s.send" % (_sckey)
+    data = {
+        "text": "%s" % (_title),
+        "desp": "%s" % (_context)
+    }
+    print("向Server酱推送消息中……")
+    try:
+        res = requests.post(url=url, data=data)
+        msg_back = json.loads(res.text)
+        print("返回值：%s" % (msg_back["errmsg"]))
+    except Exception:
+        print("消息发送错误")
+
+
 def clean():
     if sys_type == "Windows":
         os.system("cls")
@@ -63,13 +80,10 @@ def clean():
 #         Code Start        #
 #                           #
 #############################
-
-# 记录第几次操作
-flag = 0 
-# 判断系统类型
-sys_type = platform.system()
-# 收件邮箱（多个用,分割）
-mail_receiver = 'example@163.com,example@qq.com'
+flag = 0  # 记录第几次操作
+sys_type = platform.system()  # 判断系统类型
+mail_receiver = 'example@163.com,example@qq.com'  # 收件邮箱（多个用,分割）
+sc_key = ""
 # 商品的url
 urls = [
     'https://c0.3.cn/stock?skuId=1336984&area=16_1370_46145_0&venderId=1000078145&buyNum=1&choseSuitSkuIds=&cat=9192,12190,1517&extraParam={%22originid%22:%221%22}&fqsp=0&pdpin=&pduid=158071785741420407990&ch=1&callback=jQuery1571998',
@@ -77,9 +91,9 @@ urls = [
     'https://c0.3.cn/stock?skuId=100011293928&area=16_1370_46145_0&venderId=1000078145&buyNum=1&choseSuitSkuIds=&cat=9192,12190,1517&extraParam={%22originid%22:%221%22}&fqsp=0&pdpin=&pduid=158071785741420407990&ch=1&callback=jQuery3089493',
     'https://c0.3.cn/stock?skuId=100006248177&area=16_1370_46145_55521&venderId=1000078145&buyNum=1&choseSuitSkuIds=&cat=9192,12190,1517&extraParam={%22originid%22:%221%22}&fqsp=0&pdpin=&pduid=158071785741420407990&ch=1&callback=jQuery4539079',
     'https://c0.3.cn/stock?skuId=100006248247&area=16_1370_46145_55521&venderId=1000078145&buyNum=1&choseSuitSkuIds=&cat=9192,12190,1517&extraParam={%22originid%22:%221%22}&fqsp=0&pdpin=&pduid=158071785741420407990&ch=1&callback=jQuery8283487'
-    ]
+]
 # 每个url对应名称(方便辨认)
-names = ['D7002 蓝色 10只','H950V 白色 25只','H930V 白色 5只','D7002V 蓝色 6只','D7030V 白色 6只']
+names = ['D7002 蓝色 10只', 'H950V 白色 25只', 'H930V 白色 5只', 'D7002V 蓝色 6只', 'D7030V 白色 6只']
 
 while (1):
     try:
@@ -89,33 +103,35 @@ while (1):
             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
             "Connection": "keep-alive"
         }
-        time_now = time.strftime("%Y-%m-%d %H:%M:%S",time.localtime())
+        time_now = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
         print('### 第 ' + str(flag) + ' 次         ' + time_now)
         flag += 1
-        a = 0 # 用于匹配names列表的索引号
+        a = 0  # 用于匹配names列表的索引号
         for url in urls:
             # 商品url
             skuidUrl = 'https://item.jd.com/' + url.split('skuId=')[1].split('&')[0] + '.html'
             name = names[a]
             try:
                 response = session.get(url)
-                #print(response.text)
+                # print(response.text)
                 if (response.text.find('无货') > 0):
-                    print('无货:' +name+'：' +skuidUrl)
+                    print('无货:' + name+'：' + skuidUrl)
                 else:
-                    print('有货-有货- 有货啦!:'+name+'：  '+ skuidUrl)
-                    sendMail(name,skuidUrl,time_now)
+                    print('有货-有货- 有货啦!:'+name+'：  ' + skuidUrl)
+                    sendMail(name=name, url=skuidUrl, timestamp=time_now)  # 发送邮件进行提醒
+                    if sc_key != "":  # Server 酱的推送
+                        import json
+                        send_wxmsg(_sckey=sc_key, _title=name+"  到货提醒", _context=skuidUrl+"\n"+time_now)
                     # 有货时把响应保存一下(ps.主要还是调试用)
-                    f = open("./%s-%s.txt" % (flag,time.localtime()),"w",encoding="utf-8")
+                    f = open("./%s-%s.txt" % (flag, time.localtime()), "w", encoding="utf-8")
                     f.write(response.text)
                     f.close()
             except requests.exceptions.ConnectionError:
                 print("网络连接错误")
                 time.sleep(3)
             a = a + 1
-        time.sleep(random.randint(10,15))
+        time.sleep(random.randint(10, 15))
         clean()
     except KeyboardInterrupt:
         print("\n用户终止操作！程序退出...")
         break
-
