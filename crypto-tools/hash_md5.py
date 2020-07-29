c37edc516fed68b3886b048513f5f3122642dc96
hui-shao <31568606+hui-shao@users.noreply.github.com>
2020-07-29 15:47:17 +0800

    [Add] 新增多次md5加密

---
+++ crypto-tools/hash_md5.py
@@ -0,0 +1,13 @@
+import hashlib
+import sys
+
+str_1 = '这是测试'  # 需要加密的文本
+n = 100000000  # 加密次数
+if __name__ == '__main__':
+    str_md5 = hashlib.md5(str_1.encode(encoding='utf-8')).hexdigest()
+    for i in range(100000000 - 1):
+        str_md5 = hashlib.md5(str_md5.encode(encoding='utf-8')).hexdigest()
+        sys.stdout.write('\r%d' % i)
+        sys.stdout.flush()
+    print('\n\n')
+    print(str_md5)
